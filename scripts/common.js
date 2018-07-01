d1a0ba1179b9e9bde428a875f507ea2d82fd61a7
Mesabloo <ghilain.60@gmail.com>
2018-07-01 12:10:47 +0200

    Worked on the about part.

--- scripts/common.js
+++ scripts/common.js
@@ -4,3 +4,10 @@ $(window).on('unload', e => {
         $('body').css('opacity', 1)
     }, 500)
 })
+
+$('a-link, base-link').click(e => {
+    $('body').toggleClass('end')
+    setTimeout(() => {
+        window.location.href = `${e.target.attributes.href.value}`
+    }, 500)
+})
